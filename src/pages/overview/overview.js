138b9adfc43c0011e5febea179ce5412ace4292c
陈小祥 <chenxiaoxiang@CXX-MacMini.local>
2019-05-27 22:56:06 +0800

    :zap: 导入nprogress

--- src/pages/overview/overview.js
+++ src/pages/overview/overview.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './overview.less';
+import NProgress from 'nprogress';
 class Overview extends React.Component {
+    componentDidMount() {
+        NProgress.start();
+    }
     render() {
         return <div>overview!!!</div>;
     }
