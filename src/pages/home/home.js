79de8c7c1cd03edf7eb10520110e030c0b357718
陈小祥 <chenxiaoxiang@CXX-MacMini.local>
2019-05-28 16:13:14 +0800

    :zap: 使用state来选中当前url对应的菜单

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -7,51 +7,61 @@ const { Header, Content, Footer, Sider } = Layout;
 class Home extends React.Component {
   constructor(props) {
     super();
+    this.state = {currentSelectedIndex: '0'}
   }
+  
+  componentWillMount() {
+    const currentPathName = this.props.location.pathname;
+    const currentSelectedIndex = this.props.childs.findIndex(child => child.path === currentPathName);
+    this.setState({
+      currentSelectedIndex: `${currentSelectedIndex}`
+    })
+  }
+  
   menuSelected(event) {
     this.props.history.push(this.props.childs[+event.key].path);
   }
   render() {
-      return <Layout>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onBreakpoint={broken => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
-        >
-          <div className="logo">记账本</div>
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['0']}  onSelect={(event) => {
-            this.menuSelected(event);
-          }}>
+    return <Layout>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={broken => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="logo">记账本</div>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={[this.state.currentSelectedIndex]}  onSelect={(event) => {
+          this.menuSelected(event);
+        }}>
+          {this.props.childs.map((child, index) => 
+            <Menu.Item key={index}>
+              <Icon type={child.icon} />
+              <span className="nav-text">{child.title}</span>
+            </Menu.Item>
+          )},
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header style={{ background: '#fff', padding: 0 }}>
+          <div className="user-info">
+            <img src={process.env.PUBLIC_URL + '/imgs/header/photo-default.jpg'} alt="头像" />
+            <span>陈小祥</span>
+          </div>
+        </Header>
+        <Content style={{ margin: '24px 16px 0' }}>
+          <div style={{ padding: 24, background: '#fff', minHeight: 360, height: '100%' }}> 
             {this.props.childs.map((child, index) => 
-              <Menu.Item key={index}>
-                <Icon type={child.icon} />
-                <span className="nav-text">{child.title}</span>
-              </Menu.Item>
-            )},
-          </Menu>
-        </Sider>
-        <Layout>
-          <Header style={{ background: '#fff', padding: 0 }}>
-            <div className="user-info">
-              <img src={process.env.PUBLIC_URL + '/imgs/header/photo-default.jpg'} alt="头像" />
-              <span>陈小祥</span>
-            </div>
-          </Header>
-          <Content style={{ margin: '24px 16px 0' }}>
-            <div style={{ padding: 24, background: '#fff', minHeight: 360, height: '100%' }}> 
-              {this.props.childs.map((child, index) => 
-                <Route key={index} exact={child.exact} path={child.path} component={child.component}/>
-              )}
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>记账本 ©2019 Created by Cxx</Footer>
-        </Layout>
+              <Route key={index} exact={child.exact} path={child.path} component={child.component}/>
+            )}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>记账本 ©2019 Created by Cxx</Footer>
       </Layout>
+    </Layout>
   }
 }
 export default withRouter(Home);
