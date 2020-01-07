d4941ae4aefa61cce7a829fb46835dbb8622bbc8
candy4390 <2459402883@qq.com>
2020-01-07 10:54:30 +0800

    d3力导图,水波纹

--- src/pages/overview/overview.js
+++ src/pages/overview/overview.js
@@ -2,51 +2,117 @@ import React from 'react';
 import './overview.less';
 import nprogressHoc from '../../components/nprogress/nprogress';
 import * as d3 from 'd3';
+import data from '../../locales/miserables.json';
 class Overview extends React.Component {
+    // 水波纹效果
     drawChart() {
-        var dataset = [],
-            i = 0;
+        var width = Math.max(960, window.innerWidth),
+            height = Math.max(500, window.innerHeight);
 
-        for (i = 0; i < 5; i++) {
-            dataset.push(Math.round(Math.random() * 100));
+        var i = 0;
+
+        var svg = d3.select("#viz").append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        svg.append("rect")
+            .attr("width", width)
+            .attr("height", height)
+            .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
+
+        function particle() {
+            var m = d3.mouse(this);
+
+            svg.insert("circle", "rect")
+                .attr("cx", m[0])
+                .attr("cy", m[1])
+                .attr("r", 1e-6)
+                .style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5))
+                .style("stroke-opacity", 1)
+                .transition()
+                .duration(2000)
+                .ease(Math.sqrt)
+                .attr("r", 100)
+                .style("stroke-opacity", 1e-6)
+                .remove();
+
+            d3.event.preventDefault();
         }
-        var sampleSVG = d3.select("#viz")
-            .append("svg")
-            .attr("width", 402)
-            .attr("height", 82);
-            sampleSVG.selectAll("circle")
-            .data(dataset)
-            .enter().append("circle")
-            .style("stroke", "gray")
-            .style("fill", "white")
-            .attr('r', 40)
-            .attr("cx", function(d, i){return i*80 + 40})
-            .attr("cy", 40)
-            .on("mouseover", function () { d3.select(this).style("fill", "aliceblue"); })
-            .on("mouseout", function () { d3.select(this).style("fill", "white"); })
-            .on("mousedown", animateFirstStep);
+    }
 
-        function animateFirstStep() {
-            console.log('-----');
-            d3.select(this)
-                .transition()
-                .delay(0)
-                .duration(1000)
-                .attr("r", 10)
-                .each(animateSecondStep);
-        };
+    // 力导图
+    drawForce() {
+        const width = 600;
+        const height = 600;
+        const links = data.links;
+        const nodes = data.nodes;
+        const drag = simulation => {
+            function dragstarted(d) {
+                if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+                d.fx = d.x;
+                d.fy = d.y;
+            }
+            function dragged(d) {
+                d.fx = d3.event.x;
+                d.fy = d3.event.y;
+            }
+            function dragended(d) {
+                if (!d3.event.active) simulation.alphaTarget(0);
+                d.fx = null;
+                d.fy = null;
+            }
+            return d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended);
+        }
 
-        function animateSecondStep() {
-            console.log('second step');
-            d3.select(this)
-                .transition()
-                .delay(1000)
-                .duration(1000)
-                .attr("r", 40);
-        };
+        var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+        const simulation = d3.forceSimulation(nodes)
+            .force("link", d3.forceLink(links).id(d => d.id))
+            .force("charge", d3.forceManyBody())
+            .force("center", d3.forceCenter(width / 2, height / 2));
+
+        const svg = d3.select('#viz').append("svg")
+            .attr("viewBox", [0, 0, width, height]);
+
+        const link = svg.append("g")
+            .attr("stroke", "#999")
+            .attr("stroke-opacity", 0.6)
+            .selectAll("line")
+            .data(links)
+            .join("line")
+            .attr("stroke-width", d => Math.sqrt(d.value));
+        const node = svg.append("g")
+            .attr("stroke", "#fff")
+            .attr("stroke-width", 1.5)
+            .selectAll("circle")
+            .data(nodes)
+            .join("circle")
+            .attr("r", 5)
+            .attr("fill", d => color(d.group))
+            .call(drag(simulation));
+
+        node.append("title")
+            .text(d => d.id);
+
+        simulation.on("tick", () => {
+            link
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
+
+            node
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+        });
+
+        // invalidation.then(() => simulation.stop());
     }
     componentDidMount() {
-        this.drawChart();
+        this.drawForce();
     }
     render() {
         return <div id="viz"></div>;
