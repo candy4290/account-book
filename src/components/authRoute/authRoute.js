9a60fcba877f285f17be8d830d2d1483f96381ae
candy4390 <2459402883@qq.com>
2020-01-10 15:02:18 +0800

    解决render方法应该为纯函数的报错

--- src/components/authRoute/authRoute.js
+++ src/components/authRoute/authRoute.js
@@ -6,22 +6,23 @@ import { CONSTANTS } from '../../utils/constant';
 import { message } from 'antd';
 export default class AuthRoute extends React.Component {
     render() {
-        if (this.props.auth) {
-            const token = localStorage.getItem(CONSTANTS.ACCESS_TOKEN);
-            if (token && !isTokenExpired(token)) {
-                return <Route render={() => (
-                    <this.props.component {...this.props} />
-                )} />;
-            } else {
-                message.error('请先登录！');
-                localStorage.removeItem(CONSTANTS.ACCESS_TOKEN); // token过期，将其移除
-                localStorage.setItem(CONSTANTS.REDIRECT_URL, window.location.pathname)
-                // 未登录跳转到登录页面
-                return <Redirect to='/login' />
-            }
+        const token = localStorage.getItem(CONSTANTS.ACCESS_TOKEN);
+        const tokenValid = token && !isTokenExpired(token);
+        if (this.props.auth && tokenValid) {
+            return <Route render={() => (
+                <this.props.component {...this.props} />
+            )} />;
+                
+        } else if (this.props.auth && !tokenValid) {
+            message.error('请先登录！');
+            localStorage.removeItem(CONSTANTS.ACCESS_TOKEN); // token过期，将其移除
+            localStorage.setItem(CONSTANTS.REDIRECT_URL, window.location.pathname)
+            // 未登录跳转到登录页面
+            return <Redirect to='/login' />
+        } else {
+            return <Route render={() => (
+                <this.props.component {...this.props} />
+            )} />;
         }
-        return <Route render={() => (
-            <this.props.component {...this.props} />
-        )} />;
     }
 }
